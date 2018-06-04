68f4eba6f8c85a3ae2cca6c634e90f48de6eb75e
mjtedder <mjtedder@gmail.com>
2018-06-04 16:14:40 -0400

    added psuedocode for customer app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,9 @@
+//required node packages
 require("dotenv").config()
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+//creat connection to DB
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -10,6 +12,7 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+//connecting to DB
 connection.connect(function (err) {
             if (err) throw err;
             //TESTING CONNECTION TO SQL
@@ -19,3 +22,28 @@ connection.connect(function (err) {
             })
         });
     
+//display list of available products from the DB
+
+//show the list to the customer and allow them to select a product
+
+        //after product is selected, show details
+
+        //use inquirer to ask quantity
+
+        //check requested quantity vs stock
+        //if requested quantity is too high, reduce request and comfirm
+
+
+        //if customer agrees, complete transaction, else cancel transaction
+
+        //if quantity is acceptable, make the sale
+
+
+    // show the customer the total cost of the sale, and confirm
+
+    //if customer purchases, update stock on DB
+
+        //else, cancel transaction
+
+
+    //close connection
