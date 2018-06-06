bdeb38c2ae5c695a1e6cf36d4355ee6c33634893
mjtedder <mjtedder@gmail.com>
2018-06-06 16:33:56 -0400

    User can view products from db, address for loop

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -31,17 +31,49 @@ function afterConnection() {
         for (let i = 0; i < results.length; i++) {
             console.log("ID: " + results[i].item_id + " | " + "Product: " + results[i].product_name + " | " + "Department: " + results[i].department_name + " | ");
             console.log("=====================================================================");
+            displayProducts();
         }
 
     });
 }
 
 //show the list to the customer and allow them to select a product
-
-//after product is selected, show details
-
-//use inquirer to ask quantity
-
+function displayProducts() {
+    //query the database for all items being auctioned
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        //once you have the items, prompt user for which item they want to buy
+        inquirer
+            .prompt([{
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].product_name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "Which item would you like to purchase?"
+                },
+                //use inquirer to ask quantity
+                {
+                    name: "quantity",
+                    type: "input",
+                    message: "How many would you like to purchase?"
+                }
+            ])
+            //after product is selected, show details 
+            .then(function (answer) {
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].product_name === answer.choice) {
+                        chosenItem = results[i];
+                    }
+                }
+            });
+    });
+}
 //check requested quantity vs stock
 //if requested quantity is too high, reduce request and comfirm
 
